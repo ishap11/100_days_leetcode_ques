bd07f8ae18f0653f8399ab57f01bb5aeb35782c3
Isha Prajapati <128213933+ishap11@users.noreply.github.com>
2023-10-23 20:42:23 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (91.80%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        while(n>=4){
+            if(n%4 !=0) return false;
+            n =n/4;
+        }
+        return n== 1;
+    }
+}
