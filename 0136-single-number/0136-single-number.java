e269dbdeb1de177776d84639297909fbc3d9966f
Isha Prajapati <128213933+ishap11@users.noreply.github.com>
2023-11-03 21:45:56 +0530

    Time: 6 ms (38.08%), Space: 44.5 MB (37.94%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int i = 0;
+        while(i<nums.length-1){
+            if(nums[i]==nums[i+1]){
+                i = i+2;
+            }
+            else{
+                break;
+            }
+            
+        }
+        return nums[i];
+    }
+}
