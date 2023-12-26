5345cb336e04f51304377e166e97958176d33120
Isha Prajapati <128213933+ishap11@users.noreply.github.com>
2023-12-26 15:57:40 +0530

    Time: 0 ms (100.00%), Space: 41 MB (5.03%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isHappy(int n) {
+        if (n == 1 || n == 7)
+            return true;
+        int sum = n, x = n;
+ 
+        // this loop executes till the sum of square of
+        // digits obtained is not a single digit number
+        while(sum > 9) {
+            sum = 0;
+ 
+            // this loop finds the sum of square of digits
+            while (x > 0) {
+                int d = x%10;
+                sum += d*d;
+                x/=10;
+            }
+            if (sum == 1)
+                return true;
+            x = sum;
+        }
+        if(sum == 7)
+            return true;
+        return false;
+    }
+}
