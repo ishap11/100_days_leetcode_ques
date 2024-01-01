7e92803f33943a3c07094affb13c5d6b6bfd9bdb
Isha Prajapati <128213933+ishap11@users.noreply.github.com>
2024-01-01 21:47:31 +0530

    Time: 2 ms (13.37%), Space: 44.5 MB (37.74%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.java
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.java
@@ -1,42 +1,42 @@
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
         // TC - O(n) SC - O(1)
-      int[] arr = new int[nums.length];
-        int i=0;
-        int j=arr.length-1;
-        for(int num:nums){
-            if(num%2==0) arr[i++] = num;
-            else arr[j--] = num;
-        }
-        return arr;
+      // int[] arr = new int[nums.length];
+      //   int i=0;
+      //   int j=arr.length-1;
+      //   for(int num:nums){
+      //       if(num%2==0) arr[i++] = num;
+      //       else arr[j--] = num;
+      //   }
+      //   return arr;
 
 
     // TC - O(n) Sc - O(1)
-        // int i=0;
-        // int j= nums.length-1;
+        int i=0;
+        int j= nums.length-1;
 
-        // while(i<= j){
-        //     // first odd last even
-        //     if(nums[i]% 2== 1 && nums[j]%2 == 0 ){
-        //         int temp = nums[i];
-        //         nums[i] = nums[j];
-        //         nums[j] = temp;
-        //     }
-        //     // first even last odd
-        //     else if(nums[i]% 2== 0 && nums[j]%2 == 1){
-        //         i++;
-        //         j--;
-        //     }
-        //     // first even last even
-        //     else if (nums[i]% 2== 0 && nums[j]%2 == 0){
-        //         i++;
-        //     }
-        //     // last odd first odd
-        //     else{
-        //         j--;
-        //     }
-        // }
-        // return nums;
+        while(i<= j){
+            // first odd last even
+            if(nums[i]% 2== 1 && nums[j]%2 == 0 ){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+            // first even last odd
+            else if(nums[i]% 2== 0 && nums[j]%2 == 1){
+                i++;
+                j--;
+            }
+            // first even last even
+            else if (nums[i]% 2== 0 && nums[j]%2 == 0){
+                i++;
+            }
+            // last odd first odd
+            else{
+                j--;
+            }
+        }
+        return nums;
     }
 
 
