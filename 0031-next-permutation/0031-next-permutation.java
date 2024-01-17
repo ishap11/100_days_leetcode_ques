6fc4126aeedc4f5a0a833463e3bc6374a3f81c47
Isha Prajapati <128213933+ishap11@users.noreply.github.com>
2024-01-17 23:12:25 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (32.10%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,49 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+          int n = nums.length;// size of the array.
+
+        // Step 1: Find the break point:
+        int ind = -1; // break point
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                // index i is the break point
+                ind = i;
+                break;
+            }
+        }
+
+        // If break point does not exist:
+        if (ind == -1) {
+            // reverse the whole array:
+            reverse(nums, 0, n - 1);
+            return;
+        }
+
+        // Step 2: Find the next greater element
+        //         and swap it with arr[ind]:
+
+        for (int i = n - 1; i > ind; i--) {
+            if (nums[i] > nums[ind]) {
+                swap(nums, i, ind);
+                break;
+            }
+        }
+
+        // Step 3: reverse the right half:
+        reverse(nums, ind + 1, n - 1);
+
+    }
+    void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    }
+
+    void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
