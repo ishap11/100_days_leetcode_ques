0f80274db8872be523b76b78112dd1a4232fa945
Isha Prajapati <128213933+ishap11@users.noreply.github.com>
2024-01-18 16:35:33 +0530

    Time: 1 ms (99.93%), Space: 57.1 MB (66.95%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,5 +1,19 @@
 class Solution {
     public int maxSubArray(int[] nums) {
+        // BRUTE FORCE  T(c)- O(n^3) Sc - O(1)
+        // int ans = Integer.MIN_VALUE;
+        // for(int i=0; i< nums.length ;i++){
+        //     for(int j= i ; j< nums.length ; j++){
+        //         int sum =0;
+        //         for(int k= i ; k< j; k++){
+        //             sum += nums[k];
+        //         }
+        //         ans = Math.max(sum , ans);
+        //     }
+        // }
+        // return ans;
+
+//         Optimised Approach TC - O(N) SC - O(1)
         int sum =0;
         int ans = Integer.MIN_VALUE;
 
