8c15a263d7b2df3ea943ddcd0c91054b151df8ec
Isha Prajapati <128213933+ishap11@users.noreply.github.com>
2024-02-15 12:28:37 +0530

    Time: 2 ms (77.25%), Space: 50.7 MB (22.02%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,22 +1,23 @@
 class Solution {
     public int majorityElement(int[] nums) {
+        /*
         // Brute froce - O(N^2) SC - O(1)
-        // for(int i =0  ;i< nums.length  ;i++){
-        //     int count = 0;
-        //     for(int j = 0 ; j < nums.length ; j++){
-        //         if(nums[i]== nums[j]){
-        //             count++;
-        //         } 
-        //     }
-        //     // check if frquency is greater than n/2:
-        //     if(count > nums.length/2){
-        //         return  nums[i];
-        //     }
-        // }
-        // return  -1;
+        for(int i =0  ;i< nums.length  ;i++){
+            int count = 0;
+            for(int j = 0 ; j < nums.length ; j++){
+                if(nums[i]== nums[j]){
+                    count++;
+                } 
+            }
+            // check if frquency is greater than n/2:
+            if(count > nums.length/2){
+                return  nums[i];
+            }
+        }
+        return  -1;
+        */
 
-
-
+        /*
         // Better approach - Hasmap 
         // TC - O(N*logN) SC - O(N) as we are using a map data structure.
 
@@ -39,5 +40,26 @@ class Solution {
             }
         }
         return -1;
+        */
+
+
+        // OPTIMISED APPROSCH =>MOORE VOTING ALGO TC- O(N) SC - O(1)
+
+        int val = nums[0];
+        int count =1;
+
+        for(int i=1; i< nums.length ;i++){
+            if(nums[i] == val){
+                count++;
+            }else{
+                count--;
+               
+            }
+             if(count == 0){
+                val = nums[i];
+                count =1;
+            }
+        }
+        return val;
     }
 }
