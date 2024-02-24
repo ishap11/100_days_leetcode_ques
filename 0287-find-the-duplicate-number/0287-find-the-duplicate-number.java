3820386ae356ca0d8d660241797c0c1b83d80d64
Isha Prajapati <128213933+ishap11@users.noreply.github.com>
2024-02-25 00:15:47 +0530

    Time: 4 ms (87.34%), Space: 58.1 MB (60.50%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -11,7 +11,7 @@ class Solution {
         // }
         // return 0;
 
-
+        /*
         // TC - O(n) SC - O(n)
         int n =  nums.length;
         int freq[] = new int[n + 1];
@@ -23,7 +23,21 @@ class Solution {
             }
         }
         return 0;
+        */
 
+        int slow = nums[0];
+        int fast = nums[0];
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        fast = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
 
     }
 }
