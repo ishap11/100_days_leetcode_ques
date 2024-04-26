042ada7dbae718e2a7c2be739e69e11edad37234
Isha Prajapati <128213933+ishap11@users.noreply.github.com>
2024-04-26 22:30:22 +0530

    Time: 13 ms (20.57%), Space: 45 MB (87.14%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,16 +1,39 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        Arrays.sort(nums);
-        int i = 0;
-        while(i<nums.length-1){
-            if(nums[i]==nums[i+1]){
-                i = i+2;
-            }
-            else{
-                break;
-            }
+        // Arrays.sort(nums);
+        // int i = 0;
+        // while(i<nums.length-1){
+        //     if(nums[i]==nums[i+1]){
+        //         i = i+2;
+        //     }
+        //     else{
+        //         break;
+        //     }
             
+        // }
+        // return nums[i];
+        
+        int n  = nums.length;
+        HashMap<Integer, Integer> mpp = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            int value = mpp.getOrDefault(nums[i], 0);
+            mpp.put(nums[i], value + 1);
         }
-        return nums[i];
+
+        //Find the single element and return the answer:
+        for (Map.Entry<Integer, Integer> it : mpp.entrySet()) {
+            if (it.getValue() == 1) {
+                return it.getKey();
+            }
+        }
+        return -1;
+        
+        
+        // METHOd-3
+        // int a =0;
+        // for(int i=0 ; i<nums.length ; i++){
+        //     a = a^nums[i];
+        // }
+        // return a;
     }
 }
