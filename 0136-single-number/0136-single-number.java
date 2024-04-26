9817541825661f21dd32c5e32c404e640edf8f50
Isha Prajapati <128213933+ishap11@users.noreply.github.com>
2024-04-26 22:30:51 +0530

    Time: 1 ms (99.86%), Space: 46.1 MB (42.31%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -13,27 +13,27 @@ class Solution {
         // }
         // return nums[i];
         
-        int n  = nums.length;
-        HashMap<Integer, Integer> mpp = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            int value = mpp.getOrDefault(nums[i], 0);
-            mpp.put(nums[i], value + 1);
-        }
+//         int n  = nums.length;
+//         HashMap<Integer, Integer> mpp = new HashMap<>();
+//         for (int i = 0; i < n; i++) {
+//             int value = mpp.getOrDefault(nums[i], 0);
+//             mpp.put(nums[i], value + 1);
+//         }
 
-        //Find the single element and return the answer:
-        for (Map.Entry<Integer, Integer> it : mpp.entrySet()) {
-            if (it.getValue() == 1) {
-                return it.getKey();
-            }
-        }
-        return -1;
+//         //Find the single element and return the answer:
+//         for (Map.Entry<Integer, Integer> it : mpp.entrySet()) {
+//             if (it.getValue() == 1) {
+//                 return it.getKey();
+//             }
+//         }
+//         return -1;
         
         
         // METHOd-3
-        // int a =0;
-        // for(int i=0 ; i<nums.length ; i++){
-        //     a = a^nums[i];
-        // }
-        // return a;
+        int a =0;
+        for(int i=0 ; i<nums.length ; i++){
+            a = a^nums[i];
+        }
+        return a;
     }
 }
